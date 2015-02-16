12c4a25996cb807500e872c1c5850b0c47070e3a
HargisJ <hargisj@ripon.edu>
2015-02-16 14:44:22 -0600

    Updated conversion files

--- Main/source/Conversion64.cpp
+++ Main/source/Conversion64.cpp
@@ -15,10 +15,16 @@ void LongLongToDouble(long long int i, double& d)
 	d = *dPtr;
 }
 
+//Convert a 64 bit integer to a double using division
+void LongLongToDoubleDivide(long long int i, double& d)
+{
+	d = double(i) / (pow(2,64) - 1);
+}
+
 //Test an integer to see if its bits will translate to a double between 0 and 1
 bool TestInt(long long int i)
 {
-	if((i & 0x3FF0000000000000 && i & 0x000FFFFFFFFFFFFF) || i == 0)
+	if(((i & 0x3FF0000000000000) ^ 0x3FF0000000000000) || i == 0 || i == 0x3FF0000000000000)
 	{
 		return true;
 	}else{
@@ -33,4 +39,50 @@ int BitsRequired(long long int i)
 	return int(d);
 }
 
+
+//Function definitions for dieRoll class
+dieRoll::dieRoll(){
+	current_Index = 0;
 }
+
+dieRoll::dieRoll(unsigned long long int(*function)()){
+	current_Index = 0;
+	get_64_Int = function;
+	current_Long_Long = get_64_Int();
+	breakUp();
+}
+
+dieRoll::~dieRoll(){
+
+}
+
+void dieRoll::setFunction(unsigned long long int (*function)()){
+	get_64_Int = function;
+}
+
+unsigned int dieRoll::getNext(){
+	if(current_Index < 21){
+		current_Index++;
+		return roll[current_Index - 1];
+	}else{
+		current_Long_Long = get_64_Int();
+		breakUp();
+		current_Index = 0;
+		return roll[current_Index];
+	}
+}
+
+void dieRoll::breakUp(){
+	for(int i = 0; i < 21; i++){
+		roll[i] = 7 & (current_Long_Long >> (3*i));
+		//roll[i] = current_Long_Long;
+	}
+}
+
+unsigned long long int dieRoll::get_current_Long(){
+	return current_Long_Long;
+}
+
+
+
+} //Namespace SSPRNG
--- Main/source/Conversion64.h
+++ Main/source/Conversion64.h
@@ -6,10 +6,38 @@ namespace SSPRNG
 
 void LongLongToDouble(long long int i, double& d);
 
+void LongLongToDoubleDivide(long long int i, double& d);
+
 bool TestInt(long long int i);
 
 int BitsRequired(long long int i);
 
+//
+class dieRoll{
+public:
+	dieRoll();
+	dieRoll(unsigned long long int (*)());
+
+	~dieRoll();
+
+	void setFunction(unsigned long long int (*)());
+
+	unsigned int getNext();
+
+	unsigned long long int get_current_Long();
+
+	unsigned int current_Index;
+
+
+private:
+	unsigned long long int (*get_64_Int)();
+
+	unsigned long long int current_Long_Long;
+
+	unsigned int roll[21];
+
+	void breakUp();
+};
 
 }
 
